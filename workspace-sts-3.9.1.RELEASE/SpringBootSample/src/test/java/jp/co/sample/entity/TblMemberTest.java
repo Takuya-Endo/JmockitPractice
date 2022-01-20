c970ca2391785b7b2c0ab9f6e7484356259b7aeb
Takuya-Endo <above5purple@gmail.com>
2022-01-20 22:22:10 +0900

    [Update]JmockitPractice-12

---
+++ workspace-sts-3.9.1.RELEASE/SpringBootSample/src/test/java/jp/co/sample/entity/TblMemberTest.java
@@ -0,0 +1,239 @@
+package jp.co.sample.entity;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class TblMemberTest {
+
+	@BeforeEach
+	void setUp() throws Exception {
+	}
+
+	@Test
+	void testHashCode() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGetSexType() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGetDeleteType() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGetSearchMemberName() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGetAgeLower() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGetAgeUpper() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGetSearchPositionId() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGetSearchPlaceId() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGetMemberId() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGetMemberName() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGetAge() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGetSexFlg() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGetAddress() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGetTelephone() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGetMail() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGetPositionId() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGetPositionName() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGetPlaceId() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGetPlaceName() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGetRegistDate() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGetUpdateDate() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGetDeleteFlg() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testSetSearchMemberName() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testSetAgeLower() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testSetAgeUpper() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testSetSearchPositionId() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testSetSearchPlaceId() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testSetMemberId() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testSetMemberName() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testSetAge() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testSetSexFlg() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testSetAddress() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testSetTelephone() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testSetMail() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testSetPositionId() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testSetPositionName() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testSetPlaceId() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testSetPlaceName() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testSetRegistDate() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testSetUpdateDate() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testSetDeleteFlg() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testEqualsObject() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testCanEqual() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testToString() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testTblMember() {
+		fail("Not yet implemented");
+	}
+
+}

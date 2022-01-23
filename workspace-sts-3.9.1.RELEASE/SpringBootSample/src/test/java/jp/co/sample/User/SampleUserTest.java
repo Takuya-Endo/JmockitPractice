f55044e75e3f5856b421e77102bc5dd96a8b6f36
Takuya-Endo <above5purple@gmail.com>
2022-01-24 04:07:42 +0900

    [Update]JmockitPractice-18

---
+++ workspace-sts-3.9.1.RELEASE/SpringBootSample/src/test/java/jp/co/sample/User/SampleUserTest.java
@@ -0,0 +1,19 @@
+package jp.co.sample.User;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class SampleUserTest {
+
+	@BeforeEach
+	void setUp() throws Exception {
+	}
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}

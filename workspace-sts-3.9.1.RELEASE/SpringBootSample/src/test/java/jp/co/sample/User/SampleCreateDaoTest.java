5140a62ef620a18100bbd0eb15343f7a4f7fbb8e
Takuya-Endo <above5purple@gmail.com>
2022-01-28 07:37:17 +0900

    [Update]JmockitPractice-23

--- workspace-sts-3.9.1.RELEASE/SpringBootSample/src/test/java/jp/co/sample/User/SampleCreateDaoTest.java
+++ workspace-sts-3.9.1.RELEASE/SpringBootSample/src/test/java/jp/co/sample/User/SampleCreateDaoTest.java
@@ -1,12 +1,14 @@
 package jp.co.sample.User;
 
-import static org.junit.jupiter.api.Assertions.*;
-
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import mockit.Mocked;
+
 class SampleCreateDaoTest {
 
+	@Mocked private SampleDao sampleDao;
+	
 	@BeforeEach
 	void setUp() throws Exception {
 	}
